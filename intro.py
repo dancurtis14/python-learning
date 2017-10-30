a4c33b848e59346c270a684e10dcceb9639b7d36
Daniel Curtis <daniel.john.curtis-cic-uk@ibm.com>
2017-10-30 09:29:14 +0000

    Update file

---
+++ intro.py
@@ -0,0 +1,22 @@
+x = 4
+print("Hello")
+print (0==1)
+
+if 6>5:
+    print("Six is greater than 5")
+
+if "banana" in "bananarama":
+    print("I miss the 80\'s")
+
+your_list = ["a", "b", "c"]
+print(type(your_list))
+print(len(your_list))
+your_list.append("d")
+print(your_list)
+
+for letter in your_list:
+    print(letter)
+
+ice_cream = {"Alice" : "chocolate", "Beth" : "strawberry", "Chris" : "vanilla"}
+ice_cream["Alice"]
+print (ice_cream["Alice"])
