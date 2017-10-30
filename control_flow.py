2a509d1c78e0758526cdb70b418954e2b61a9d5d
Daniel Curtis <daniel.john.curtis-cic-uk@ibm.com>
2017-10-30 15:52:18 +0000

    Added

---
+++ control_flow.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 15:46:40 2017
+
+@author: GB121361
+"""
+
+age = 35
+name = "Megan"
+
+if age == 35:
+    if name == "Megan":    
+        print(name + " is 35")
+else:
+    print("Not 35")
