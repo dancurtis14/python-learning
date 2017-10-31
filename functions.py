5f802fb59a7350ffe66f6afa7229678ba44c1745
Daniel Curtis <daniel.john.curtis-cic-uk@ibm.com>
2017-10-31 12:17:14 +0000

    update

---
+++ functions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 12:11:54 2017
+
+@author: GB121361
+"""
+
+PI  = 3.141592
+
+def circle_area(r):
+    return PI*r*r
+
+print(circle_area(5))
+print(circle_area(7))
+print(circle_area(9))
