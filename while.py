2bd8ed125db2445b1dc19ecb734222b3d2075987
Daniel Curtis <daniel.john.curtis-cic-uk@ibm.com>
2017-10-31 12:12:05 +0000

    l

---
+++ while.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 11:36:25 2017
+
+@author: GB121361
+"""
+
+index = 0 
+names = ["Josh", "Harry", "Leah", "Micah"]
+
+while index < len(names):
+    name = names[index]
+    print(name)
+    index = index+1
+
+total = 0
+v = 1 
+while v <= 10:
+    total = total + v
+    v = v + 1
+    
+print(total)
+
